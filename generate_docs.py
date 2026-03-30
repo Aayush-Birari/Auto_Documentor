876227c057359707c382b5a7bb33951330559804
Aayush-Birari <aayush.birari12@gmail.com>
2026-03-30 20:22:48 +0530

    Fixed merge conflicts and cleaned script

--- generate_docs.py
+++ generate_docs.py
@@ -1,10 +1,7 @@
 import os
 from groq import Groq
 
-# Jenkins will inject the key we saved
 api_key = os.getenv("GEMINI_API_KEY") 
-
-# Initialize the Groq client
 client = Groq(api_key=api_key)
 
 def generate_readme(code_content):
@@ -16,14 +13,8 @@ def generate_readme(code_content):
     {code_content}
     """
     
-    # Using Meta's Llama 3 model via Groq (Super fast, no billing traps)
     chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": prompt,
-            }
-        ],
+        messages=[{"role": "user", "content": prompt}],
         model="llama-3.1-8b-instant", 
     )
     return chat_completion.choices[0].message.content
@@ -38,8 +29,4 @@ if __name__ == "__main__":
     with open("README.md", "w") as file:
         file.write(readme_content)
 
-<<<<<<< HEAD
     print("README.md successfully updated!")
-=======
-    print("README.md successfully updated!")
->>>>>>> 51ef4d2c03f2faa4476aa988884757f498ef2a95
